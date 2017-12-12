342dca7c6948d7699b8cdcb9b9fafe6097e74a94
Hammed Noibi <hnobi08@yahoo.com>
2017-12-11 21:17:17 -1100

    create todoitems iside todo

--- server/controllers/todos.js
+++ server/controllers/todos.js
@@ -1,4 +1,5 @@
 const Todo = require('../models').Todo;
+const TodoItem = require('../models').TodoItem;
 
 module.exports = {
   create(req, res) {
@@ -12,7 +13,13 @@ module.exports = {
 
   list(req, res) {
     return Todo
-      .all()
+      //.all()
+      .findAll({
+        include: [{
+          model: TodoItem,
+          as: 'todoItems',
+        }],
+      })
       .then(todos => res.status(200).send(todos))
       .catch(error => res.status(400).send(error));
   },
--- server/models/todo.js
+++ server/models/todo.js
@@ -1,17 +1,4 @@
-// 'use strict';
-// module.exports = (sequelize, DataTypes) => {
-//   var Todo = sequelize.define('Todo', {
-//     title: DataTypes.STRING
 
-//   }, {
-//       classMethods: {
-//         associate: function (models) {
-//           // associations can be defined here
-//         }
-//       }
-//     });
-//   return Todo;
-// };
 module.exports = (sequelize, DataTypes) => {
   const Todo = sequelize.define('Todo', {
     title: {
