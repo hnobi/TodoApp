9e16159e5bce6d0698780269700202218aa6da84
Hammed Noibi <hnobi08@yahoo.com>
2017-12-15 09:24:28 -1100

    create update and delete routes and controller

--- server/controllers/todos.js
+++ server/controllers/todos.js
@@ -43,6 +43,50 @@ module.exports = {
         return res.status(200).send(todo);
       })
       .catch(error => res.status(200).send(error));
-  }
+  },
+
+
+  update(req, res) {
+    return Todo
+      .findById(req.params.todoId, {
+        include: [{
+          model: TodoItem,
+          as: 'todoItems',
+        }],
+      })
+      .then(todo => {
+        if (!todo) {
+          return res.status(404).send({
+            message: 'Todo Not Found',
+          });
+        }
+        return todo
+          .update({
+            title: req.body.title || todo.title,
+          })
+          .then(() => res.status(200).send(todo))  // Send back the updated todo.
+          .catch((error) => res.status(400).send(error));
+      })
+      .catch((error) => res.status(400).send(error));
+  },
+
+  destroy(req, res) {
+    return Todo
+      .findById(req.params.todoId)
+      .then(todo => {
+        if (!todo) {
+          return res.status(400).send({
+            message: 'Todo Not Found',
+          });
+        }
+        return todo
+          .destroy()
+          .then(() => res.status(200).send({ message: 'Todo deleted successfully.' }))
+          .catch(error => res.status(400).send(error));
+      })
+      .catch(error => res.status(400).send(error));
+  },
+
+
 
 };
--- server/routes/index.js
+++ server/routes/index.js
@@ -10,4 +10,15 @@ module.exports = (app) => {
     app.get('/api/todos', todosController.list);
     app.post('/api/todos/:todoId/items', todoItemsController.create);
     app.get('/api/todos/:todoId', todosController.retrieve);
+    app.put('/api/todos/:todoId', todosController.update);
+    app.delete('/api/todos/:todoId', todosController.destroy);
+    app.put('/api/todos/:todoId/items/:todoItemId', todoItemsController.update);
+    app.delete('/api/todos/:todoId/items/:todoItemId', todoItemsController.destroy);
+
+
+    // For any other request method on todo items, we're going to return "Method Not Allowed"
+    app.all('/api/todos/:todoId/items', (req, res) =>
+        res.status(405).send({
+            message: 'Method Not Allowed',
+        }));
 };
